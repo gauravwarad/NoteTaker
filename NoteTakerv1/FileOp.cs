3601d675e818842be3c252a899adcfb1e5793965
gauravwarad <41246827+gauravwarad@users.noreply.github.com>
2021-04-06 18:46:59 +0530

    completed

---
+++ NoteTakerv1/FileOp.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows.Media.Imaging;
+using Xceed.Document.NET;
+using Xceed.Words.NET;
+
+namespace NoteTakerv1
+{
+    static class FileOp
+    {
+        private static string filename = "";
+        private static DocX doc;
+
+        public static void openFile()
+        {
+            doc = DocX.Load(filename);
+            doc.InsertParagraph("");
+            doc.Save();
+        }
+
+        public static void addText(string textboxip)
+        {
+            doc = DocX.Load(filename);
+            doc.InsertParagraph(textboxip);
+            doc.Save();
+        }
+
+        public static void addPicture(BitmapSource image1)
+        {
+            string imgPath = @"C:\Users\Gaurav\Desktop\banana.png";
+            using (var fileStream = new FileStream(imgPath, FileMode.Create))
+            {
+                BitmapEncoder encoder = new PngBitmapEncoder();
+                //encoder.Frames.Add(BitmapFrame.Create(image));
+                encoder.Frames.Add(BitmapFrame.Create(image1));
+                encoder.Save(fileStream);
+            }
+
+
+            var doc = DocX.Load(filename);
+            Xceed.Document.NET.Image img = doc.AddImage(imgPath);
+            Picture p = img.CreatePicture();
+            Xceed.Document.NET.Paragraph para = doc.InsertParagraph("");
+            para.AppendPicture(p);
+
+            doc.Save();
+            File.Delete(imgPath);
+        }
+        public static string launchFile()
+        {
+            try
+            {
+                Process.Start(@"C:\Program Files\Microsoft Office\Office16\WINWORD.EXE", filename);
+                return "launching";
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+                Console.WriteLine(ex.Message);
+                return ex.Message;
+            }
+        }
+        public static string getFileName()
+        {
+            return filename;
+        }
+        
+        public static void setFileName(string filename)
+        {
+            FileOp.filename = filename;
+        }
+        public static bool isNameEmpty()
+        {
+            if (filename == "")
+            
+                return true;
+            
+            else
+                return false;
+        }
+    }
+}
--- NoteTakerv1/MainWindow.xaml.cs
+++ NoteTakerv1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Xceed.Words.NET;
+using GI.Screenshot;
+using Xceed.Document.NET;
+using System.IO;
 
 namespace NoteTakerv1
 {
@@ -20,10 +25,129 @@ namespace NoteTakerv1
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        //private string textboxip = "";
+        //private DocX doc;
+        //private string filepath = "";
         public MainWindow()
         {
             InitializeComponent();
             //first change; checking version control.
+
+        }
+
+        private void ScreenShotButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            //filepath = FileOp.getFileName();
+
+            if (FileOp.isNameEmpty())
+            {
+                RemarksLabel.Content = "please select a file first";
+            }
+            else
+            {
+                BitmapSource image1 = Screenshot.CaptureAllScreens();
+                //BitmapFrame bf = BitmapFrame.Create(image);
+
+                //Stream logo = new MemoryStream();
+                FileOp.addPicture(image1);
+
+                RemarksLabel.Content = "screenshotted";
+                //MainTextBox.Clear();
+            }
+
+        }
+
+        private void SnipButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (FileOp.isNameEmpty())
+            {
+                RemarksLabel.Content = "please select a file first";
+            }
+            else
+            {
+                //textboxip = MainTextBox.Text;
+                BitmapSource image1 = Screenshot.CaptureRegion();
+
+                FileOp.addPicture(image1);
+
+                RemarksLabel.Content = "snipping";
+                //MainTextBox.Clear();
+            }
+
+        }
+
+        private void PushTextButton_Click(object sender, RoutedEventArgs e)
+        {
+            string textboxip;
+
+            if(FileOp.isNameEmpty())
+            {
+                RemarksLabel.Content = "please select a file first";
+            }
+            else
+            {
+                textboxip = MainTextBox.Text;
+                //doc = DocX.Load(filepath);
+                FileOp.addText(textboxip);
+                RemarksLabel.Content = "pushed";
+                MainTextBox.Clear();
+            }
+            
+
+        }
+
+        private void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+            
+            //i'm using only word documents, so..
+            openFileDialog.Filter = "Word Documents|*.docx";
+            
+            //did the dialog open up; stored in response; nullable bool
+            bool? response = openFileDialog.ShowDialog();
+
+            if(response == true)
+            {
+                //filename stored here, in filepath
+                string filepath = openFileDialog.FileName;
+                FileOp.setFileName(filepath);
+
+                RemarksLabel.Content = "selected file is" + filepath;
+
+                FileOp.openFile();
+
+            }
+
+            if(FileOp.isNameEmpty()) //empty filepath
+            {
+                RemarksLabel.Content = "please select a file";
+            } 
+
+        }
+
+        private void LaunchFileButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (FileOp.isNameEmpty())
+            {
+                RemarksLabel.Content = "please select a file first";
+            }
+            else
+            {
+                string filepath = FileOp.getFileName();
+                //RemarksLabel.Content = filepath;
+                //Console.WriteLine(filepath);
+                //string filename = @"C:\Users\Gaurav\Desktop\forNoteTaker.docx";
+
+                string op = FileOp.launchFile();
+
+                RemarksLabel.Content = op;
+                //MainTextBox.Clear();
+            }
+
         }
     }
 }
